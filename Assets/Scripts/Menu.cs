c6724c2224623a1d97052913bbb51ffcce4c366b
Cwlrin <20304773@qq.com>
2022-06-28 16:03:23 +0800

    主菜单 / Main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void UiEnable()
+    {
+        GameObject.Find("Canvas/MainMenu/UI").SetActive(true);
+    }
+}
